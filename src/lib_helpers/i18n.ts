11d096326cb1a33aa6922dcfc2205d5d733552a5
pinkyrabbit <usesa@yandex.com>
2024-09-29 20:12:29 +0200

    fix apply lang types

--- src/lib_helpers/i18n.ts
+++ src/lib_helpers/i18n.ts
@@ -3,9 +3,9 @@ import { i18n as i18n_buttons } from "../buttons";
 import { Message } from "node-telegram-bot-api";
 import { Lang } from "../constants";
 
-export const applyLang = (lang: string, msg?: Message) => {
+export const applyLang = (lang: Lang, msg?: Message) => {
   const locales = i18n_content.getLocales();
-  const toApply: Lang = locales.includes(lang) ? lang as Lang : Lang.RU;
+  const toApply: Lang = locales.includes(lang) ? lang as Lang : Lang.EN;
   i18n_content.setLocale(toApply);
   i18n_buttons.setLocale(toApply);
   if (msg?.user) {
